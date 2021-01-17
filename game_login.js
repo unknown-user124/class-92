a4cf48406b94d442d54fe992dd185cb41e5b6292
unknown-user124 <73541799+unknown-user124@users.noreply.github.com>
2021-01-17 11:58:27 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function gotoAPP() {
+    player_1_name=document.getElementById("player1_input").value;
+    player_2_name=document.getElementById("player2_input").value;
+    localStorage.setItem("player_1_name",player_1_name);
+    localStorage.setItem("player_2_name", player_2_name);
+    window.location="game_page.html";
+}
---
+++ game_page.js
@@ -0,0 +1,90 @@
+player_1_name=localStorage.getItem("player_1_name");
+player_2_name=localStorage.getItem("player_2_name");
+
+player_1_score= 0;
+player_2_score= 0;
+
+document.getElementById("player1name").innerHTML=player_1_name+" :";
+document.getElementById("player2name").innerHTML=player_2_name+" :";
+document.getElementById("player1score").innerHTML=player_1_score;
+document.getElementById("player2score").innerHTML=player_2_score;
+document.getElementById("playerquestion").innerHTML="Question turn-"+player_1_name;
+document.getElementById("playeranswer").innerHTML="Answer turn-"+player_2_name;
+
+function send() {
+    get_word=document.getElementById("word").value;
+    word=get_word.toLowerCase();
+    console.log("word in lowerCase="+word);
+
+    charAt1=word.charAt(1);
+    console.log(charAt1);
+
+    length_divide_2=Math.floor(word.length/2);
+    charAt2=word.charAt(length_divide_2);
+    console.log(charAt2);
+
+    
+    length_minus_1=word.length-1;
+    charAt3=word.charAt(length_minus_1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+     remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+      remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+       console.log(remove_charAt3); 
+
+       question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
+        input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+         check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+          row = question_word + input_box + check_button ;
+           document.getElementById("output").innerHTML = row;
+     document.getElementById("word").value = "";
+
+}
+question_turn="player1";
+answer_turn="player2";
+
+function check() {
+ getAnswer=document.getElementById("input_check_box").value;
+the_Answer=getAnswer.toLowerCase();
+console.log(the_Answer);
+
+if (the_Answer==word) {
+    if (answer_turn=="player1") {
+        player_1_score=player_1_score+1;
+        document.getElementById("player1score").innerHTML=player_1_score;
+        
+       
+    }
+    else {
+        player_2_score=player_2_score+1;
+        document.getElementById("player2score").innerHTML=player_2_score;
+
+    }
+
+}
+if (question_turn=="player1") {
+    question_turn="player2";
+    document.getElementById("playerquestion").innerHTML="Question turn-"+player_2_name;
+    
+    
+}
+else {
+    question_turn="player1"; 
+    document.getElementById("playerquestion").innerHTML="Question turn-"+player_1_name;
+    
+}
+ 
+if (answer_turn=="player1") {
+    answer_turn="player2";
+    document.getElementById("playeranswer").innerHTML="Answer turn-"+player_2_name;
+    
+    
+}
+else {
+    question_turn="player1"; 
+    document.getElementById("playeranswer").innerHTML="Answer turn-"+player_1_name;
+    
+}
+document.getElementById("output").value = "";
+}
